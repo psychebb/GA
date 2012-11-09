68282e4ed0510242e1a32afa618f77e9bab0d75f
psychebb <psychebb@163.com>
2012-11-09 14:45:50 +0800

    modified the coding part, return a cell as the coding result of a F

--- coding.m
+++ coding.m
@@ -8,38 +8,19 @@ f = [f,zero];  %补0
 F = cell(n,1);
 
 for i = 1:1:n
-    k=0; %编码后的index
-	flag = 0;%有没有遇到1
-    p = 1;%表示1后的第几个0
+    k=1; %编码后的index
     sub = m-(d(i)-1)*distance;
     f1 = [];
-	for j = 1:1:m+distance
+    j = 1;
+	while j <= m+distance
 		if f(i,j) == 0
-			if flag == 0%说明没有遇见1，则编码为0
-				f1(1,k+1) = 0;
-				k = k +1;
-			else 
-				if p == distance
-					if isempty(f1) ~= 1  
-                        f1(1,k+1) = 1;
-                        flag = 0;
-                        p = 0;
-                    else
-                        f1(1,k+1) = 1;
-                        flag = 0;
-                        p = 0;
-                    end
-                else
-                    p = p+1;
-				end
-			end
-        else
-            if k == 0
-                flag = 1;
-            else
-                flag = 1;
-                k = k+1;
-            end
+			f1(1,k) = 0;
+			k = k +1;
+            j = j+1;
+		else 
+			f1(1,k) = 1;
+            j = j+5;
+            k = k+1;
         end
     end
     F{i,1} = f1;
