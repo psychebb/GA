1ce4c31f7c8cd3a176c437a7e9e7b14c1aea2c7b
psychebb <psychebb@163.com>
2012-11-09 15:15:17 +0800

    change the decoding part, return a row as decoding result of solution matrix F

--- decoding.m
+++ decoding.m
@@ -2,19 +2,19 @@
 function f = decoding(f,c)
 [n m] = size(f);
 distance = c(1,1)-1;
-for i = 1:1:n
-	k = 1;
-	for j = 1:1:m
-		if f(i,j) == 1
-			f1(i,k) = 1;
-			for l = k+1:1:k+distance
-				f1(i,l) = 0;
-			end
-			k = k + distance + 1
-		else
-			f1(i,k) = 0;
-			k = k + 1;
+f1 = [];
+k = 1;
+for j = 1:1:m
+	if f(1,j) == 1
+		f1(1,k) = 1;
+		for l = k+1:1:k+distance
+			f1(1,l) = 0;
 		end
+		k = k + distance + 1;
+	else
+		f1(1,k) = 0;
+		k = k + 1;
     end
 end
-f = f1(:,1:m-distance);
+[a b] = size(f1);
+f = f1(:,1:b-distance);
