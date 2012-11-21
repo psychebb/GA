12c7f016c04f49c67a2f03a3efa5645fdeffc49b
psychebb <psychebb@163.com>
2012-11-21 16:17:12 +0800

    initialize the f randomly, and meet the demand and shortest CSC, the distance between the notes random

--- random_allocate.m
+++ random_allocate.m
@@ -1,8 +1,13 @@
-function [f,d]=random_allocate(f,index,d)
+%对于有些小区在分配时不能满足需求 将剩余的需求随机分配
+%index表示第几个小区（即第几行） demand为这个小区的需求 random为产生的随机位置
+%分配的结果是得到第index小区满足需求且满足最小间隔的随机分配
+function [f,d]=random_allocate(f,index,d,c)
+    distance = c(1,1)-1;
 	[n m] = size(f);
 	demand = d(index);
-	random = randperm(m);
-	for j:1:demand
+	random = random_location(demand,m,distance);
+	for j=1:demand
 		f(index,random(j)) = 1;
 	end
 	f;
+    demand = 0;
